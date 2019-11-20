c7839dc9741f70899eb61aa4ed6c02add04c19f9
Artem Kuryakov <kurik01tagil@gmail.com>
2019-11-20 08:25:46 +0300

    fixed for from size_t type to int

--- include/task3.h
+++ include/task3.h
@@ -3,9 +3,10 @@
 template<typename T, size_t size>
 void map(T(&arr)[size],T (*change)(T))
 {
-	for (size_t i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		arr[i] = change(arr[i]);
 	} 
+	
 	return ;
 }
